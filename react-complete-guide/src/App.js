081047ba6d327e2cf6d109f5d9197e8a2eff0143
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-11-24 12:42:57 +0100

    passing method references between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,73 +1,12 @@
-import React, { useState } from "react";
+import React, { Component } from 'react';
 
-import Person from "./Person/Person";
+import Person from './Person/Person';
 
-import "./App.css";
+import './App.css';
 
-const App = (props) => {
-  //hooks always return 2 elements
-  // first element is the state
-  // second is a function which allows us to update the state
-  const [personsState, setPersonsState] = useState({
-    persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 },
-    ]
-  });
-
-  const [otherState, setOtherState] = useState({otherState: 'some other value'});
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    //console.log("Was clicked!");
-    // DON'T DO THIS!  this.state.persons[0].name = 'Maximillian'
-    // has {} in it's argument, merges everything we put inside with our existing state
-    // rerendering caused only by changing state and props
-    setPersonsState({
-      // doesn't merge previous and updated state, instead it replaces 
-      // previous and updated state
-      // we have to update non-updated things manually
-      // instead use multiple states
-      persons: [
-        { name: "Maximilian", age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 },
-      ]
-    });
-  };
-
-  console.log(personsState)
-
-  //this reffers to a class
-  //don't add parentheses! it will run automatically after rendering!
-
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      >
-        My hobby is: racing
-      </Person>
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
-};
-
-export default App;
-
-/*
+class App extends Component {
+  // stateful, smart or container, compontent -> manages state
+  // good to have only a couple of them
   state = {
     persons: [
       { name: 'Max', age: 28 },
@@ -78,16 +17,41 @@ export default App;
   }
 
   // if we don't use arrow function, possible problems with "this"
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("Was clicked!");
-    // DON'T DO THIS!  this.state.persons[0].name = 'Maximillian'
+    // DON'T DO THIS!  this.state.persons[0].name = 'Maximilian'
     // has {} in it's argument, merges everything we put inside with our existing state
     // rerendering caused only by changing state and props
     this.setState({persons: [
-      { name: 'Maximilian', age: 28 },
+      { name: newName, age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 27 }
     ]
   });
   }
-*/
+
+  //this reffers to a class
+  //don't add parentheses! it will run automatically after rendering!
+  // <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>  CAN BE INEFITIENT
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')} >My hobby is: racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Person = (props) => {
+  // stateless, dumb or prezentational, component, doesn't manage state
+  // good to have a lot of them, easy to change and manage
   // {} for dynamic content in a jsx file
   // children reffers to any element between opening and closing tag
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         I am a {props.name} and I am {props.age} years old.
       </p>
       <p>{props.children}</p>
