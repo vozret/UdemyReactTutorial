4bd351caa47c43da592d7460276f4e3620b6e86d
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-11-25 11:11:29 +0100

    flexible lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   // good to have only a couple of them
   state = {
     persons: [
-      { id:'sasasa', name: "Max", age: 28 },
-      { id:'kakaka', name: "Manu", age: 29 },
-      { id:'lalala', name: "Stephanie", age: 26 },
+      { id: "sasasa", name: "Max", age: 28 },
+      { id: "kakaka", name: "Manu", age: 29 },
+      { id: "lalala", name: "Stephanie", age: 26 },
     ],
     otherState: "some other value",
     showPersons: false,
@@ -19,13 +19,25 @@ class App extends Component {
 
   // if we don't use arrow function, possible problems with "this"
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    // ALWAYS WORK WITH THE COPIES OF THE STATE!!!
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+      // left: state, right: new copy
+      persons: persons,
     });
   };
 
@@ -37,15 +49,15 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({
-      persons: persons
+      persons: persons,
     });
-  }
+  };
 
   //togglePersonsHandler(){} problems with 'this'
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow });
   };
 
   //this reffers to a class
@@ -63,16 +75,19 @@ class App extends Component {
     let persons = null;
 
     // index element in the map() is passed here automatically
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-            name={person.name}
-            age={person.age}
-            click={() => this.deletePersonHandler(index)}
-            key={person.id}
-          />
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
@@ -86,8 +101,8 @@ class App extends Component {
         </button>
         {persons}
       </div>
-
     );
-}}
+  }
+}
 
 export default App;
