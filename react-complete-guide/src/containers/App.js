a376eb4166642163ca5e54e055f79e4432f8548e
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-12-01 11:16:24 +0100

    component update lifecycle for state change

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -33,6 +33,15 @@ class App extends Component {
     console.log('[App.js] ComponentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] ComponentDidUpdate');
+  }
+
   // if we don't use arrow function, possible problems with "this"
 
   nameChangedHandler = (event, id) => {
