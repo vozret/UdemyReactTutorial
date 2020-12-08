e42880ce8fa6e321573c2e06d5f29ccfe92e4b91
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-12-08 16:22:17 +0100

    connecting state to build controls

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,31 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
 
 class BurgerBuilder extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            ingredients: {
-                salad: 0,
-                bacon: 0,
-                cheese: 0,
-                meat: 0
-            }
-        };
-    }
+    this.state = {
+      ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+      },
+      totalPrice: 4,
+    };
+  }
 
-    render() {
-        return(
-            <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
-            </Aux>
-        );
-    }
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCounted = oldCount + 1;
+
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCounted;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    });
+  };
+
+  removeIngredientHandler = (type) => {};
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+        />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
