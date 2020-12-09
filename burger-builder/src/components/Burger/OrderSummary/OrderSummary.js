b3f3f7732a406a0b44a927fea7e159aec3094aa1
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-12-09 23:30:58 +0100

    adding a price to the order summary

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Aux from "../../../hoc/Aux";
-import Button from '../../UI/Button/Button';
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,12 +16,17 @@ const orderSummary = (props) => {
     <Aux>
       <h3>Your order</h3>
       <p>A delicious burger with the fillowing ingredients:</p>
-      <ul>
-          {ingredientSummary}
-      </ul>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
