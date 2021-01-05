9f76ce32f0bea1342f89fab2d7995d0b3664f420
Veronika Ozretic <vozret00@fesb.hr>
2021-01-05 18:17:44 +0100

    Improving Perfomance

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,32 +3,40 @@ import React from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends React.Component {
+  // this could be a functional component, doesn't have to be a class
+  componentDidUpdate() {
+    console.log("[OrderSummary] did update")
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burger with the fillowing ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burger with the fillowing ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
+  }
+
 };
 
-export default orderSummary;
+export default OrderSummary;
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -4,21 +4,33 @@ import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
+class Modal extends React.Component {
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  } 
+
+  componentDidUpdate() {
+    console.log("[Modal] did update");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+
 };
 
-export default modal;
+export default Modal;
