4fec29ad974ce59d1abf6f2319bece3db3b17951
Veronika Ozretic <vozret00@fesb.hr>
2021-01-06 12:30:15 +0100

    transforming data

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -16,7 +16,14 @@ class Blog extends Component {
         // get method returns a promise
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
                 // console.log(response);
             });
     }
@@ -24,7 +31,7 @@ class Blog extends Component {
     render () {
         const posts = this.state.posts.map(
             post => {
-                return <Post key={post.id} title={post.title} />;
+                return <Post key={post.id} title={post.title} author={post.author} />;
             }
         );
 
