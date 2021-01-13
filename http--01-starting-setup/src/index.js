20a5945ba067babfb2804647133e8633e2f74e19
Veronika Ozretic <vozret00@fesb.hr>
2021-01-13 11:47:56 +0100

    setting a default global configuration for axios

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = 'application/json';
+
 // interceptors are used for inputing a header (e.g. authorization header)
 // usually request -> config
 // need to return request otherwise the interceptor blocks the request
