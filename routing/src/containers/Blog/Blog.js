b3045c6ed03db4fa58b4a3618df4debc3060eb05
Veronika Ozretic <vozret00@fesb.hr>
2021-02-04 19:33:43 +0100

    Using Links to switch pages

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -12,8 +12,11 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            {/* Link prevents the default behavior (reloading the page)
+                                and handles the click on itself
+                            */}
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
